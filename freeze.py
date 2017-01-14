c5e0a8696a6560a6e1b9f6769f1fb8697993edb6
Ilya V. Schurov <ilya@schurov.com>
2017-01-14 03:36:29 +0300

    fixing urls

--- freeze.py
+++ freeze.py
@@ -3,6 +3,7 @@ from flask_frozen import Freezer
 from pythonvjs import app
 
 freezer = Freezer(app)
+app.config['FREEZER_DESTINATION'] = 'docs/pythonvjs'
 
 if __name__ == '__main__':
     freezer.freeze()
